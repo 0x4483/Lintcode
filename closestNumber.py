302062ac0bdf76bb5706e285032c5eab178ee5ba
0x4483 <zihanguo2016@gmail.com>
2017-07-06 00:07:29 -0400

    Update closestNumber.py

--- closestNumber.py
+++ closestNumber.py
@@ -3,18 +3,19 @@ class Solution:
     # @param {int} target an integer
     # @return {int} an integer
     def closestNumber(self, A, target):
-        
         left = 0
         right = len(A) - 1
-        
-        while left + 1 < right:
+        while (left + 1 < right):
             mid = (left + right) / 2 
-            if target < A[mid]:
+            if (target < A[mid]):
                 right = mid
             else:
-                left = mid
-                    
-        if target - A[left] < A[right] - target:
+                left = mid          
+        if (target - A[left] < A[right] - target):
             return left
         else:
             return right
+
+# Time: O(log(n)) 
+# Space: O(1) 
+# Tag: Binary Search, Easy
