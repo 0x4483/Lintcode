a52db64aa02852e9e7c303422c6b5b32f8a58372
0x4483 <zihanguo2016@gmail.com>
2017-07-06 00:05:48 -0400

    Create closestNumber.py

---
+++ closestNumber.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @param {int[]} A an integer array sorted in ascending order
+    # @param {int} target an integer
+    # @return {int} an integer
+    def closestNumber(self, A, target):
+        
+        left = 0
+        right = len(A) - 1
+        
+        while left + 1 < right:
+            mid = (left + right) / 2 
+            if target < A[mid]:
+                right = mid
+            else:
+                left = mid
+                    
+        if target - A[left] < A[right] - target:
+            return left
+        else:
+            return right
